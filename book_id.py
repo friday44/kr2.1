40ef944fd2cadd9a4aa24d6f16ea4708f1620fff
friday <friday0701@mail.ru>
2023-02-27 10:30:27 +0400

    book_id edit

--- book_id.py
+++ book_id.py
@@ -3,10 +3,7 @@ import csv
 def get_id():
     with open('book.csv', "r") as data:
         reader = csv.reader(data, delimiter = ';')
-        print('hello')
-        for row in reader:
-            #print(f'row0={row[0]}')
-            max_id = max((row[0]))
-            print(f'max_id={max_id}')
+        max_id = max(int(row[0]) for row in reader)
+        next_id = max_id + 1
     
-    return max_id
+    return next_id
